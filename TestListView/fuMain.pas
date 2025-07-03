38e417f35b24d89a2241173c6bff862926b8ca78
NickBahtin <bahtin@bk.ru>
2025-07-03 10:35:54 +0300

    Signed-off-by: NickBahtin <bahtin@bk.ru>

--- TestListView/fuMain.pas
+++ TestListView/fuMain.pas
@@ -25,7 +25,6 @@ type
     btnClear: TCornerButton;
     ShadowEffect1: TShadowEffect;
     ShadowEffect5: TShadowEffect;
-    Layout2: TLayout;
     procedure btnAddTableClick(Sender: TObject);
     procedure btnAddStringClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
@@ -33,6 +32,9 @@ type
   private
     { Private declarations }
     mycol:TInitGridRecord;
+    procedure OnCellDblClick(const Column: TColumn; const Row: Integer);
+    procedure OnGetValue(Sender: TObject; const ACol, ARow: Integer;
+      var Value: TValue);
   public
     { Public declarations }
   end;
@@ -48,25 +50,74 @@ implementation
 
 procedure TForm1.btnAddStringClick(Sender: TObject);
 begin
-  AddTextToScrollBox(Layout2,btnAddString.Tag,'Результат: '+IntToStr(btnAddString.Tag)+' Компонентов:'+IntToStr(lvResult.ComponentCount+1));
+  if ScrollBox1.ComponentCount>3 then
+     AddTextToScrollBox(ScrollBox1,btnAddString.Tag,'Результат: '+IntToStr(btnAddString.Tag)+' Компонентов:'+IntToStr(ScrollBox1.ComponentCount+1)+' Позиция:'+FloatToStr(TText(ScrollBox1.Components[ScrollBox1.ComponentCount-1]).Position.y)+
+     'Idx:'+IntToStr(TText(ScrollBox1.Components[ScrollBox1.ComponentCount-1]).ComponentIndex))
+  else
+     AddTextToScrollBox(ScrollBox1,btnAddString.Tag,'Результат: '+IntToStr(btnAddString.Tag)+' Компонентов:'+IntToStr(ScrollBox1.ComponentCount+1),TTextAlign.Leading);
   btnAddString.Tag:=btnAddString.Tag+1;
+  Caption:=TText(ScrollBox1.Components[ScrollBox1.ComponentCount-1]).Text;
 end;
 
 procedure TForm1.btnAddTableClick(Sender: TObject);
 begin
-  AddTableToScrollBox(Layout2,mycol);
+  mycol.Header:='Таблица '+IntToStr(btnAddTable.Tag+1);
+  AddTableToScrollBox(ScrollBox1,mycol);
   btnAddTable.Tag:=btnAddTable.Tag+1;
 end;
 
 procedure TForm1.btnClearClick(Sender: TObject);
 begin
-  ClearTextAndGridsFromScrollBox(Layout2);
+  ClearTextAndGridsFromScrollBox(ScrollBox1);
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  SetLength(mycol.Columns,3);
+  SetLength(mycol.Columns,4);
+  mycol.RowCount:=5;
+  mycol.RowHeight:=16;
   mycol.Header:='Таблица результатов';
+  mycol.OnGetValue:=OnGetValue;
+  mycol.OnSellClick:=OnCellDblClick;
+  //
+  mycol.Columns[0].Header:='№п.п.';
+  mycol.Columns[0].width:=40;
+  mycol.Columns[0].Mask:='%d';
+  mycol.Columns[0].IsInteger:=True;
+  //
+  mycol.Columns[1].Header:='Объем по эталону,л';
+  mycol.Columns[1].width:=150;
+  mycol.Columns[1].Mask:='%8.4f';
+  mycol.Columns[1].IsInteger:=False;
+  //
+  mycol.Columns[2].Header:='Объем по поверяемому,л';
+  mycol.Columns[2].width:=150;
+  mycol.Columns[2].Mask:='%8.4f';
+  mycol.Columns[2].IsInteger:=False;
+  //
+  mycol.Columns[3].Header:='Погрешность,%';
+  mycol.Columns[3].width:=100;
+  mycol.Columns[3].Mask:='%4.2f';
+  mycol.Columns[3].IsInteger:=False;
 end;
+
+procedure TForm1.OnCellDblClick(const Column: TColumn; const Row: Integer);
+begin
+  //
+end;
+
+procedure TForm1.OnGetValue(Sender: TObject; const ACol, ARow: Integer;
+  var Value: TValue);
+  var s:string;
+begin
+  //
+//  if mycol.Columns[ACol].IsInteger then
+   if ACol=0 then
+     Value:=Format(mycol.Columns[ACol].Mask, [ARow+1])
+   else
+     Value:=Format(mycol.Columns[ACol].Mask, [(ARow+1)*Random(10)/3]);
+end;
+
+
 
 end.
--- TestListView/lvHelper.pas
+++ TestListView/lvHelper.pas
@@ -13,24 +13,37 @@ type
     Header:string;
     Mask:string;
     width:Integer;
+    IsInteger:Boolean;
   end;
 
   TInitGridRecord=record
     Header:string;
     Idx:integer;
+    RowCount:Integer;
+    RowHeight:Integer;
     OnGetValue:TOnGetValue;
     OnSellClick:TCellClick;
     Columns:array of TInitColumnRecord;
   end;
 
-function AddTableToScrollBox(lv: TLayout;columns:TInitGridRecord):TGrid;
-function AddTextToScrollBox(lv: TLayout;Idx:integer;aValue:String):TText;
-procedure ClearTextAndGridsFromScrollBox(lv: TLayout);
+function AddTableToScrollBox(lv: TScrollBox;columns:TInitGridRecord):TGrid;
+function AddTextToScrollBox(lv: TScrollBox;Idx:integer;aValue:String;AHorzAlign: TTextAlign=TTextAlign.Trailing):TText;
+procedure ClearTextAndGridsFromScrollBox(lv: TScrollBox);
 
 implementation
 
-function AddTableToScrollBox(lv: TLayout;columns:TInitGridRecord):TGrid;
-var ic:TIntegerColumn;
+uses
+  System.Rtti;
+
+procedure OnGetValue(Sender: TObject; const ACol, ARow: Integer;
+  var Value: TValue);
+begin
+//
+end;
+
+function AddTableToScrollBox(lv: TScrollBox;columns:TInitGridRecord):TGrid;
+var i:integer;
+    ic:TIntegerColumn;
     fc:TFloatColumn;
     header:TText;
 begin
@@ -39,12 +52,46 @@ begin
   result.Parent:=lv;
   result.Tag:=columns.Idx;
   result.Margins.Top:=1;
+  result.Margins.Bottom:=10;
   result.Align:=TAlignLayout.Top;
   result.OnGetValue:=columns.OnGetValue;//заполнение клеток
   result.OnCellDblClick:=columns.OnSellClick;//на случай активации функции отключения каких то результатов
+  result.Options:=result.Options+[TGridOption.RowSelect,TGridOption.AlwaysShowSelection];
+  result.Options:=result.Options-[TGridOption.Editing, TGridOption.AlwaysShowEditor,TGridOption.ColumnResize,TGridOption.ColumnMove];
+  result.RowCount:=columns.RowCount;
+  result.RowHeight:=columns.RowHeight;
+  result.Height:=(columns.RowCount+1)*(result.RowHeight+2)+5;
+  //столбцы
+  //первый столбец, всегда номер записи
+  for I := 1 to Length(columns.Columns) do
+  begin
+     if columns.Columns[i-1].IsInteger then
+     begin
+        ic:=TIntegerColumn.Create(result);
+        ic.Parent:=Result;
+        ic.HorzAlign:=TTextAlign.Center;
+        ic.HeaderSettings.TextSettings.HorzAlign:=TTextAlign.Center;
+        ic.Width:=columns.Columns[i-1].width;
+        ic.Header:=columns.Columns[i-1].Header;
+        if i=1 then begin
+          ic.Enabled:=False;
+          ic.Locked:=True;
+        end;
+     end
+     else begin
+        fc:=TFloatColumn.Create(result);
+        fc.Parent:=Result;
+        fc.HorzAlign:=TTextAlign.Center;
+        fc.HeaderSettings.TextSettings.HorzAlign:=TTextAlign.Center;
+        fc.Width:=columns.Columns[i-1].width;
+        fc.Header:=columns.Columns[i-1].Header;
+        if i=1 then fc.Enabled:=False;
+     end;
+  end;
   //Добавляем заголовок
   header:=TText.Create(lv);
-  header.Margins.Top:=1;
+  header.Margins.Top:=5;
+  header.Margins.Bottom:=1;
   header.Text:=columns.Header;
   header.Height:=14;
   header.Parent:=lv;
@@ -54,34 +101,26 @@ begin
 end;
 
 
-function AddTextToScrollBox(lv: TLayout;Idx:integer;aValue:String):TText;
+function AddTextToScrollBox(lv: TScrollBox;Idx:integer;aValue:String;AHorzAlign: TTextAlign):TText;
 var
   txt: TText;
   LastTxt: TText;
+  LastGrid: TGrid;
   Offset: Single;
 begin
+ // Определяем позицию для нового грида
   result:=TText.Create(lv);
   result.Parent:=lv;
+  result.Align:=TAlignLayout.Top;
   result.Margins.Top:=1;
+  result.Margins.Bottom:=1;
   result.Tag:=Idx;
   result.Height:=14;
-  result.Align:=TAlignLayout.Top;
-  result.TextSettings.HorzAlign:=TTextAlign.Trailing;
+  result.TextSettings.HorzAlign:=AHorzAlign;
   result.Text:=aValue;
- // Определяем позицию для нового грида
-  if lv.ChildrenCount > 0 then
-  begin
-    LastTxt := lv.Children[Lv.ChildrenCount - 1] as TText;
-    Offset := LastTxt.Position.Y + LastTxt.Height;
-  end
-  else
-  begin
-    Offset := 0;
-  end;
-  LastTxt.Position.Y := Offset+10;
 end;
 
-procedure ClearTextAndGridsFromScrollBox(lv: TLayout);
+procedure ClearTextAndGridsFromScrollBox(lv: TScrollBox);
 var i:integer;
 begin
   for I := lv.ComponentCount downto 1 do
